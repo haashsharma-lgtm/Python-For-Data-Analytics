60d79dc6a3c14c667fcf44dd7f3d8389451acd8b
haashsharma-lgtm <haashsharma@gmail.com>
2026-08-12 19:08:04 +0530

    Add files via upload

---
+++ session_2_python_syntax.py
@@ -0,0 +1,52 @@
+# Task 1
+user_name = "Harsh"
+fav_app = "Instagram"
+daily_usage_hours = 3
+
+print(user_name)
+print(fav_app)
+print(daily_usage_hours)
+
+
+# Task 2
+product_name = "Laptop"
+price = 50000
+is_available = True
+
+print(product_name, type(product_name))
+print(price, type(price))
+print(is_available, type(is_available))
+
+
+# Task 3 - Spotify Playlist Recommendation System
+
+# This is a single-line comment.
+
+"""
+This is a multi-line comment.
+A Spotify playlist recommendation system can analyze
+the user's favorite songs, artists, and listening history
+to recommend similar songs and create personalized playlists.
+"""
+
+print("Spotify recommends songs based on your listening history.")
+
+
+
+# Task 4 - Zomato Order
+
+order_total = 1200
+delivery_region = "Surat"
+discount_percent = 10
+
+print(f"Your order total is: {order_total} and delivery region is: {delivery_region}. You have received a discount of {discount_percent}% on your order.")
+
+
+# Task 5 - Fix the indentation
+
+temperature = 30
+
+if temperature > 25:
+    print("It's a hot day.")
+else:
+    print("It's a cool day.")
